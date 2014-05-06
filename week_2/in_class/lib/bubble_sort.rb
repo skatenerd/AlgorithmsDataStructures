99bafd16ed99b9823931b793035ba49957052464
Patrick Gombert <patrickgombert@gmail.com>
2014-05-06 18:46:54 -0500

    Add bubble sort in class work for week 2

---
+++ week_2/in_class/lib/bubble_sort.rb
@@ -0,0 +1,14 @@
+class BubbleSort
+  attr_accessor :list
+
+  def initialize(list)
+    self.list = list
+  end
+
+  def tick
+  end
+
+  private
+
+  attr_accessor :loop_index, :movement_index
+end
