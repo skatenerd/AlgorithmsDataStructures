7ca0d22a32ae7c440731f6eb6d391ee775be498d
Wai Lee Chin Feman <skatenerd@gmail.com>
2014-05-16 08:14:01 -0500

    Phone number search homework

--- week_3/homework/lib/phone_number_search.rb
+++ week_3/homework/lib/phone_number_search.rb
@@ -1,4 +1,15 @@
+require_relative '../../in_class/lib/tree_traversal'
+
 class PhoneNumberSearch
+  def self.enumerate_strings(phone_number)
+  end
+
+  def self.build_tree(remaining_numbers, letters_so_far="")
+  end
+
   def self.subwords_of(composite, dictionary)
+  end
+
+  def self.find_word_breakdowns(phone_number, dictionary)
   end
 end
--- week_3/homework/spec/phone_number_search_spec.rb
+++ week_3/homework/spec/phone_number_search_spec.rb
@@ -1,28 +1,64 @@
 require 'set'
+require 'phone_number_search'
 
 describe PhoneNumberSearch do
-  before(:each) do
-    @dictionary = Set.new(["bean", "beans", "skills", "kills"])
+  describe ".build_tree" do
+    xit "builds a tree representing potential words" do
+      phone_number = [2,3]
+      tree = PhoneNumberSearch.build_tree(phone_number)
+      tree.children.count.should == 3
+      leaves = tree.children[0].children.map(&:value) +
+        tree.children[1].children.map(&:value) +
+        tree.children[2].children.map(&:value)
+      leaves.should =~ [
+        "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"
+      ]
+    end
   end
 
-  it "knows when a string is not made up of any legal words" do
-    expect(PhoneNumberSearch.subwords_of("ixjlkwifh", @dictionary)).to eq([])
+  describe ".enumerate_strings" do
+    xit "enumerates the potential strings of a phone number" do
+      phone_number = [2,3]
+      PhoneNumberSearch.enumerate_strings(phone_number).should =~ [
+        "a", "b", "c", "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"
+      ]
+    end
   end
 
-  it "knows when a string is made up of a repeated word" do
-    expect(PhoneNumberSearch.subwords_of("beanbean", @dictionary)).to eq([["bean", "bean"]])
+  describe ".subwords_of" do
+    before(:each) do
+      @dictionary = Set.new(["bean", "beans", "skills", "kills"])
+    end
+
+    xit "knows when a string is not made up of any legal words" do
+      expect(PhoneNumberSearch.subwords_of("ixjlkwifh", @dictionary)).to eq([])
+    end
+
+    xit "knows when a string is made up of a repeated word" do
+      expect(PhoneNumberSearch.subwords_of("beanbean", @dictionary)).to eq([["bean", "bean"]])
+    end
+
+    xit "knows when a string is made up of two words" do
+      expect(PhoneNumberSearch.subwords_of("beanskills", @dictionary)).to eq([
+                                                                             ["bean", "skills"],
+                                                                             ["beans", "kills"]
+      ])
+    end
+
+    xit "knows when a string is made up of many words" do
+      expect(PhoneNumberSearch.subwords_of("beanbeanbeansbeanbean", @dictionary)).to eq([
+                                                                                        ["bean", "bean", "beans", "bean", "bean"]
+      ])
+    end
   end
 
-  it "knows when a string is made up of two words" do
-    expect(PhoneNumberSearch.subwords_of("beanskills", @dictionary)).to eq([
-      ["bean", "skills"],
-      ["beans", "kills"]
-    ])
-  end
-
-  it "knows when a string is made up of many words" do
-    expect(PhoneNumberSearch.subwords_of("beanbeanbeansbeanbean", @dictionary)).to eq([
-      ["bean", "bean", "beans", "bean", "bean"]
-    ])
+  describe "coup de gras" do
+    xit 'foo' do
+      dictionary = Set.new(["user", "uby", "use", "ruby"])
+      breakdowns = PhoneNumberSearch.find_word_breakdowns([
+        8, 7, 3, 7, 8, 2, 9
+      ], dictionary)
+      breakdowns.should =~ [['user', 'uby'], ['use', 'ruby']]
+    end
   end
 end
