1d54880d58b5b36054227957e327022a6373478a
Wai Lee Chin Feman <skatenerd@gmail.com>
2014-05-29 23:43:29 -0500

    Refactor test

--- week_4/in_class/spec/determine_complexities_spec.rb
+++ week_4/in_class/spec/determine_complexities_spec.rb
@@ -13,24 +13,35 @@ describe DetermineComplexities do
     ScoringLinkedList.clear_counts
   end
 
-  it "Knows complexity of N Linked List Prepends" do
-    first_run_count = 1000
-    first_list = ScoringLinkedList.new
-    first_run_count.times do |i|
-      first_list.prepend(i)
+  def append_to_linked_list(n_times)
+    ScoringLinkedList.clear_counts
+    list = ScoringLinkedList.new
+    n_times.times do |i|
+      list.append(i)
     end
     actual_operation_count = ScoringLinkedList.operations_count
+    return actual_operation_count
+  end
+
+  def prepend_to_linked_list(n_times)
+    ScoringLinkedList.clear_counts
+    list = ScoringLinkedList.new
+    n_times.times do |i|
+      list.prepend(i)
+    end
+    actual_operation_count = ScoringLinkedList.operations_count
+    return actual_operation_count
+  end
+
+  it "Knows complexity of N Linked List Prepends" do
+    first_run_count = 1000
+    actual_operation_count = prepend_to_linked_list(first_run_count)
     predicted_operation_count = DetermineComplexities.linked_list_prepends(first_run_count)
     first_ratio = (actual_operation_count + 0.0) / predicted_operation_count
 
-    ScoringLinkedList.clear_counts
 
     second_run_count = 10000
-    second_list = ScoringLinkedList.new
-    second_run_count.times do |i|
-      second_list.prepend(i)
-    end
-    actual_operation_count = ScoringLinkedList.operations_count
+    actual_operation_count = prepend_to_linked_list(second_run_count)
     predicted_operation_count = DetermineComplexities.linked_list_prepends(second_run_count)
     second_ratio = (actual_operation_count + 0.0) / predicted_operation_count
 
@@ -38,37 +49,14 @@ describe DetermineComplexities do
   end
 
   it "knows the complexity of N Linked List Appends" do
-    ###########################################
-    # Append 50 elements.
-    ###########################################
     first_run_count = 50
-    first_list = ScoringLinkedList.new
-    first_run_count.times do |i|
-      first_list.append(i)
-    end
-    actual_operation_count = ScoringLinkedList.operations_count
+    actual_operation_count = append_to_linked_list(first_run_count)
     predicted_operation_count = DetermineComplexities.linked_list_appends(first_run_count)
-    ###########################################################################
-    # How many operations actually took place? How many do YOU think took place?
-    ###########################################################################
     first_ratio = (actual_operation_count + 0.0) / predicted_operation_count
 
-    ScoringLinkedList.clear_counts
-
-
-    ###########################################
-    # This time, append 1000 elements!
-    ###########################################
     second_run_count = 1000
-    second_list = ScoringLinkedList.new
-    second_run_count.times do |i|
-      second_list.append(i)
-    end
-    actual_operation_count = ScoringLinkedList.operations_count
+    actual_operation_count = append_to_linked_list(second_run_count)
     predicted_operation_count = DetermineComplexities.linked_list_appends(second_run_count)
-    ###########################################################################
-    # How many operations actually took place? How many do YOU think took place?
-    ###########################################################################
     second_ratio = (actual_operation_count + 0.0) / predicted_operation_count
 
 
